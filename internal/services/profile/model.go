c0871a818493238f466d8f641730d6589df677f7
Anton <Chipazawra@mail.ru>
2021-10-21 16:20:10 +0300

    add docker-compose

--- internal/services/profile/model.go
+++ internal/services/profile/model.go
@@ -8,6 +8,12 @@ type Profile struct {
 	TokenManager *jwtmng.Mng
 }
 
+type Template struct {
+	uuid    string
+	rawdata string
+	params  []string
+}
+
 func New(tm *jwtmng.Mng) *Profile {
 	return &Profile{TokenManager: tm}
 }
